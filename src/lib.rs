43a8c182f7af87712789c2fa951681e36a142593
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2025-10-12 17:27:48 +0545

    simplify impl

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,12 @@ mod reflink_copy {
     use std::path::PathBuf;
 
     #[pyo3::pyfunction]
-    fn reflink(src: PathBuf, dst: PathBuf) -> pyo3::PyResult<()> {
-        Ok(reflink_copy::reflink(src, dst)?)
+    fn reflink(src: PathBuf, dst: PathBuf) -> std::io::Result<()> {
+        reflink_copy::reflink(src, dst)
     }
 
     #[pyo3::pyfunction]
-    fn reflink_or_copy(src: PathBuf, dst: PathBuf) -> pyo3::PyResult<()> {
-        Ok(reflink_copy::reflink_or_copy(src, dst).map(|_| ())?)
+    fn reflink_or_copy(src: PathBuf, dst: PathBuf) -> std::io::Result<()> {
+        reflink_copy::reflink_or_copy(src, dst).map(|_| ())
     }
 }
